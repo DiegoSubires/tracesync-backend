35e8e58aee355b40f832f1a679f695ad6de79b3f
Diego Subires <diegosubires@gmail.com>
2026-06-05 13:51:50 +0200

    Corrección de rutas de producto

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -5,7 +5,8 @@ const productController = require("../controllers/product.controller");
 
 // 🟢 Solo operaciones del catálogo maestro
 router.get("/products", productController.getAllProducts);
-router.get("/products/:id", productController.getProductById);
+//router.get("/products/:id", productController.getProductById);
+router.get("/:id", productController.getProductById);
 //router.get("/debug-alignment", productController.debugProductsAlignment);
 
 module.exports = router;
