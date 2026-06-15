7f8e407660432e255baf3490f402f17b418fa527
Diego Subires <diegosubires@gmail.com>
2026-06-15 10:44:27 +0200

    Despliegue automatico (Backend)

--- src/utils/inventoryDefaults.js
+++ src/utils/inventoryDefaults.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-undef */
 // Este archivo sirve tanto para el backend como para el frontend (si usas TypeScript/JS compartido)
-export const getEmptyBatchLine = () => ({
+const getEmptyBatchLine = () => ({
   batch: "",
   quantity: 0,
   crates: 0,
@@ -8,10 +9,15 @@ export const getEmptyBatchLine = () => ({
   elapsedDays: 0,
 });
 
-export const getInitialProductState = (productInfo) => ({
+const getInitialProductState = (productInfo) => ({
   id: productInfo.id,
   alternativeDescription:
     productInfo.alternativeDescription || "Sin descripción",
   unitsPerCrate: productInfo.unitsPerCrate || 0,
   batchLines: [],
 });
+
+module.exports = {
+  getEmptyBatchLine,
+  getInitialProductState,
+};
